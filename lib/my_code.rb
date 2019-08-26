1e498b1e0842810ce24825878194a847c0969fb1
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-26 22:51:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,27 @@
 def map_to_negativize(array)
 array.map!{|i| -i}
 end
+
+def map_to_no_change()
+
+end
+
+def map_to_double(array)
+
+end
+
+def map_to_square(array)
+
+end
+
+def reduce_to_total(array)
+
+end
+
+def reduce_to_all_true(array)
+
+end
+
+def reduce_to_any_true(array)
+
+end
